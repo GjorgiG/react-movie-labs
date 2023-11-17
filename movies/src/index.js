4a122a5f882d199ad9d941128cbf08f6fb39b62d
Gjorgi <20098721@mail.wit.ie>
2023-11-17 13:02:34 +0000

    Fave movie list now stored in a context

--- movies/src/index.js
+++ movies/src/index.js
@@ -9,6 +9,7 @@ import SiteHeader from './components/siteHeader';
 import UpcomingMovies from './pages/upcomingMovies';
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
+import MoviesContextProvider from "./contexts/moviesContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,6 +26,7 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <SiteHeader />
+        <MoviesContextProvider>
         <Routes>
           <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
           <Route path="/movies/upcoming" element={<UpcomingMovies />} />
@@ -33,6 +35,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={ <Navigate to="/" /> } />
         </Routes>
+        </MoviesContextProvider>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
